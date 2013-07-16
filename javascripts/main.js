5feb0339fadf666bd2cbeaf99fa0d89f5a61b79e
Ethan Niu <slughorn99@gmail.com>
2013-07-16 14:46:58 -0500

    moved background images to new folder, implemented small js scripts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+function enlarge(photo) {
+    photo.className += " enlarge";
+    return false;
+}
+
+// function normalImg(photo) {
+// 	photo.className += " normal" 
+// }
+
+function removeClass(element, cssClass) {
+
+  // var classes = element.className.split(' ');
+
+  // var j = classes.length;
+  // while (j--) {
+  //   if (classes[j] === cssClass) {
+  //     classes.splice(j, 1);
+  //   }
+  // }
+
+  // element.className = classes.join(' ');
+
+  // document.getElementById('elementid').className = document.getElementById('elementid').className.replace('normal');
+}
+
+  // var enlarge = document.getElementById('enlarge');
+  // removeClass(enlarge, 'enlarge');
